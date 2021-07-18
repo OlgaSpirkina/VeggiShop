355a561f37342a6eec3fa3c08890a280df980468
olga <>
2021-07-18 14:43:51 +0200

    debug shopping icons and localStorage transfer of choosen products

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,6 +3,7 @@ if ($("body").data("title") === "main") {
   let placeForCards = document.getElementById('placeForCards');
   let shoppingIcons = document.getElementsByClassName('add-shopping');
   const arrForPrices = [];
+  let arrForStoredItems = [];
   let counter = 0;
   // Création du prototype contenant les attributs
   function Products(groupName, name, pricePerKilo, season, healthBenefits=[], country, img){
@@ -72,71 +73,16 @@ if ($("body").data("title") === "main") {
   const pumpkin = new Products('vegetables', 'courge', 2.9, 'automn', ['les intestins', 'la peau'], 'France', 'assets/img/pumpkin.jpg');
   // Légumineuses
   const darkbeans = new Legums('legums', 'haricots', 1.5, 'all', ['les muscles', 'les intestins'], 'France', 'assets/img/darkbeans.jpg', 55);
-  const chickpeas = new Legums('legums', 'pois chiche', 1.3, 'all', ['les muscles', 'les intestins'], 'Italie', 'assets/img/chickpeas.jpg', 60);
-  const lentils = new Legums('legums', 'lentilles vertes', 1.6, 'all', ['les muscles', 'les intestins'], 'France', 'assets/img/lentils.jpg', 20);
-  const peas = new Legums('legums', 'petit pois', 2.3, 'spring', ['le coeur', 'les intestins'], 'Espagne', 'assets/img/peas.jpg', 25);
-  const beans = new Legums('legums', 'haricots rouges', 1.9, 'all', ['les muscles', 'les intestins'], 'France', 'assets/img/beans.jpg', 60);
+  const chickpeas = new Legums('legums', 'poischiche', 1.3, 'all', ['les muscles', 'les intestins'], 'Italie', 'assets/img/chickpeas.jpg', 60);
+  const lentils = new Legums('legums', 'lentillesvertes', 1.6, 'all', ['les muscles', 'les intestins'], 'France', 'assets/img/lentils.jpg', 20);
+  const peas = new Legums('legums', 'petitpois', 2.3, 'spring', ['le coeur', 'les intestins'], 'Espagne', 'assets/img/peas.jpg', 25);
+  const beans = new Legums('legums', 'haricotsrouges', 1.9, 'all', ['les muscles', 'les intestins'], 'France', 'assets/img/beans.jpg', 60);
   // le tableau de tout les produits
   const arrOfProducts = [
     peach, cranberry, banana, pineapple, pear,
     cucomber, pepper, carrots, brocolli, pumpkin,
     darkbeans, chickpeas, lentils, peas, beans
   ];
-  // Les Prix. Au click sur le panier d'achat de chaque article:
-    // -le prix est récupéré dans le tableau arrForPrices,
-    // -le panier de la navbar est visible,
-    // -le compteur du panier sur la navbar est activé
-    // la somme des prix de tout les articles
-  const countShopping = () =>{
-    let priceValue = document.getElementsByClassName('priceValue');
-    let imageShopping = document.getElementsByClassName('card-img-top');
-    let storeIt = {}
-    let arrForStoredItems = [];
-    let shoppingIconsClicksCountArr = new Array(shoppingIcons.length);
-    for(let i=0; i<shoppingIcons.length; i++){
-      shoppingIconsClicksCountArr[i] = 0;
-      shoppingIcons[i].addEventListener('click', function(){
-        shoppingIcons[i].textContent = ++shoppingIconsClicksCountArr[i];
-        document.getElementById('number-of-items').style.visibility = 'visible';
-        document.getElementById('number-of-items').innerHTML = ++counter;
-        shoppingIcons[i].classList.add('price_'+i);
-        priceValue[i].classList.add('price_'+i);
-        imageShopping[i].classList.add('price_'+i);
-        if(shoppingIcons[i].classList.contains('price_'+i) && priceValue[i].classList.contains('price_'+i)){
-          arrForPrices.push(parseFloat(priceValue[i].innerHTML, 10));
-          displayPrices();
-            if(imageShopping[i].classList.contains('price_'+i)){
-              arrForStoredItems.push(  {
-                count: shoppingIcons[i].textContent,
-                image: arrOfProducts[i].img,
-                text: arrOfProducts[i].name,
-                unityPrice: parseFloat(arrOfProducts[i].pricePerKilo, 10),
-                totalPrice: arrOfProducts[i].pricePerKilo * parseFloat(shoppingIcons[i].textContent, 10)
-              });
-              return localStorage.setItem("storeObj", JSON.stringify(arrForStoredItems));
-            }
-        }
-          return arrForPrices;
-        })
-      }
-    }
-  const displayPrices = () =>{
-    let sum = 0;
-    sum += arrForPrices.reduce((a, b) => a + b, 0);
-    sessionStorage.setItem("sum", sum);
-    // console.log(typeof sum);
-    return sum;
-  }
-  displayPrices();
-  // la bar de recherche
-  const input = document.getElementById("searchBar");
-  input.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-    const filteredRecipes = arrOfProducts.filter(eachRecipe => {
-      return (eachRecipe.name.toLowerCase().includes(searchString));
-    });
-    displayProductsAsCards(filteredRecipes);
-  });
   // la fonction qui génére les cartes
   const displayProductsAsCards = (anyArr) =>{
     const result =  anyArr.map((item) => {
@@ -152,7 +98,7 @@ if ($("body").data("title") === "main") {
               <p class="card-text2 ${item.name}" id="paragraphSugar_${item.name}" style="display:none";></p>
               <p class="d-flex justify-content-between">
                 <button class="btn text-white colored-button ${item.name}" id="btn_${item.name}">En savoir plus</button>
-                <i id="${item.name}" class="fas fa-2x fa-shopping-cart add-shopping"></i>
+                <i id="${item.name}" class="fas fa-2x fa-shopping-cart add-shopping ${item.name}"></i>
               </p>
           </div>
         </div>`;
@@ -160,7 +106,15 @@ if ($("body").data("title") === "main") {
     placeForCards.innerHTML = result;
   }
   displayProductsAsCards(arrOfProducts);
-
+  // la bar de recherche
+  const input = document.getElementById("searchBar");
+  input.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredRecipes = arrOfProducts.filter(eachRecipe => {
+      return (eachRecipe.name.toLowerCase().includes(searchString));
+    });
+    displayProductsAsCards(filteredRecipes);
+  });
   // Le bouton voir plus voir moins
   const btnSeeMore = document.getElementById('btn-see-more');
   const legumsClass = document.getElementsByClassName('legums');
@@ -209,25 +163,30 @@ if ($("body").data("title") === "main") {
             return (eachCategory.groupName.toLowerCase().includes(buttonId));
           });
           displayProductsAsCards(categoryFilter);
-          countShopping();
+          // countShopping();
         }else if(buttonId == 'all'){
           document.getElementById('btn-see-more').style.display = 'block';
           displayProductsAsCards(arrOfProducts);
-          countShopping();
+          // countShopping();
           }
         }
     })
   }
-  countShopping();
-  // la fonction qui permet de montrer les info sur le taut de sucre. Le calcule du taut de sucre
-  // se fait dans la fonction prototype, mais l'affichage avec un event listener du parent global la div #placeForCards
+  // la fonction qui permet de montrer les info sur le taut de sucre et mettre les produits dans le panier.
+  // Le calcule du taut de sucre se fait dans la fonction prototype, mais l'affichage avec un event listener du parent global la div #placeForCards
+
+  let shoppingIconsClicksCountArr = new Array(shoppingIcons.length);
+  for(let i=0; i<shoppingIconsClicksCountArr.length; i++){
+    shoppingIconsClicksCountArr[i] = 0;
+  }
   placeForCards.onclick = function(event) {
     let buttonSugar = event.target.closest('button');
-      if(!buttonSugar){
+    let iconShopping = event.target.closest('i');
+      if(!buttonSugar && !iconShopping){
         return;
-      }else if(!placeForCards.contains(buttonSugar)){
+      }else if(!placeForCards.contains(buttonSugar) && !placeForCards.contains(iconShopping)){
         return;
-      }else{
+      }else if(placeForCards.contains(buttonSugar)){
         for(let i=0; i<arrOfProducts.length; i++){
           if(buttonSugar.classList.contains(`${arrOfProducts[i].name}`) && document.getElementById(`paragraphSugar_${arrOfProducts[i].name}`).classList.contains(`${arrOfProducts[i].name}`)){
           document.getElementById(`paragraphSugar_${arrOfProducts[i].name}`).style.display = (document.getElementById(`paragraphSugar_${arrOfProducts[i].name}`).style.display == 'none') ? 'block' : 'none';
@@ -235,8 +194,30 @@ if ($("body").data("title") === "main") {
           // the same thing but with a button cookIt arrOfProducts[i].cookIt();
         }
       }
+    // Mettre les produits dans le panier et les afficher sur la page cart.html grâce à localStorage
+      }else if(placeForCards.contains(iconShopping)){
+        document.getElementById('number-of-items').style.visibility = 'visible'; // le compteur sur la navbar est affiché et activé pour afficher la
+        document.getElementById('number-of-items').innerHTML = ++counter; // le nombre total des produits choisis
+        for(let i=0; i<shoppingIcons.length; i++){
+          for(let j=0; j<arrOfProducts.length; j++){
+            if((iconShopping.id == `${arrOfProducts[j].name}`) && (shoppingIcons[i].classList.contains(`${arrOfProducts[j].name}`))){
+              console.log(`here ${arrOfProducts[j].name}`);
+              shoppingIcons[i].textContent = ++shoppingIconsClicksCountArr[i]; // le compteur individuel de chaque produit
+              arrForStoredItems.push( // chaque produit est stocké dans un objet et chaque objet est rajouté dans un tableau
+              {
+                count: shoppingIcons[i].textContent,
+                image: arrOfProducts[j].img,
+                text: arrOfProducts[j].name,
+                unityPrice: parseFloat(arrOfProducts[j].pricePerKilo, 10),
+                totalPrice: arrOfProducts[j].pricePerKilo * parseFloat(shoppingIcons[i].textContent, 10)
+              });
+              localStorage.setItem("storeObj", JSON.stringify(arrForStoredItems)) // le tableau contenant tout les objets-produit est
+        // sauvegardé en format 'string' grâce à JSON.stringify et ce string est placé dans localStorage nommé "storeObj"
+            }
+          }
+        }
+      }
     }
-}
 // les fonctions qui concernent la page 'cart.html'
 }else{
   // Session storage
